40cd8edb360e1edae76c44b5110232ee0ba7c3f6
Shahil19 <basharshahil@gmail.com>
2022-03-30 08:51:07 +0600

    load data from local storage

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,10 +3,24 @@ import "./Cart.css";
 
 const Cart = (props) => {
   const { cart } = props;
+  let totalPrice = 0;
+  let totalShipping = 0;
+
+  for (const product of cart) {
+    totalPrice = totalPrice + product.price;
+    totalShipping = totalShipping + product.shipping;
+  }
+
+  let tax = parseFloat((totalPrice * 0.1).toFixed(2));
+  const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h3>Order Summary </h3>
       <p>Selected Item: {cart.length}</p>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax}</p>
+      <h4>Grand Total: ${grandTotal}</h4>
     </div>
   );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,26 +1,57 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetch("products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
 
   // ! module 48-7
   // use state for order summary
   const [cart, setCart] = useState([]);
+
+  //! useEffect is used when we load(fetch) data from outside
+  useEffect(() => {
+    // console.log("started - products", products);
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(" End - Products ");
+        setProducts(data);
+      });
+  }, []);
+
+  // load Data from local storage
+  useEffect(() => {
+    // console.log("Started - localStorage");
+    const storedCart = getStoredCart();
+    // console.log(storedCart);
+
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+
+      setCart(savedCart);
+      console.log(savedCart);
+    }
+    // console.log("finished - localStorage");
+  }, [products]);
 
   // ?----------- sent event handler as attribute and get as object value (props)
   const handleAddToCart = (product) => {
     // ?------------- newCart = cart + product (...cart = only cart property)
     const newCart = [...cart, product];
     setCart(newCart);
-    console.log(cart);
+
+    // from local storage
+    addToDb(product.id);
   };
 
   return (
